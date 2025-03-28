fdcfc20ff59e7658216ba9e2c16f970364596430
brinley53 <brinleyhull53@ku.edu>
2025-03-27 20:03:27 -0500

    enemy procedural generation (basic)

---
+++ mr-fictitious/procedural_generation.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var enemy_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Generate all items, enemies, rooms, etc here
+	generate_enemies()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func generate_enemies():
+	# Function to procedurally generate enemies in the scene
+	var rand = RandomNumberGenerator.new()
+	for i in range(3): # How many enemies to spawn
+		var enemy = enemy_scene.instantiate()
+		
+		# Randomly position and place enemy. Change range to fit screen/valid spaces
+		var spawn_x = rand.randi_range(100, 1000)
+		var spawn_y = rand.randi_range(100, 1000)
+		enemy.position = Vector2(spawn_x, spawn_y)
+		
+		add_child(enemy) # Add enemy to scene
+		
