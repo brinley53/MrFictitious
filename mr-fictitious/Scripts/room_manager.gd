1233b413fc1e3596ff780c2cde9de4001857b45d
seanhammell <sean.m.hammell@gmail.com>
2025-04-16 20:36:20 -0500

    RoomManager overhaul to support multiple locations

--- mr-fictitious/Scripts/room_manager.gd
+++ mr-fictitious/Scripts/room_manager.gd
@@ -6,7 +6,13 @@ Revisions:
 """
 extends Node2D
 
-enum PathDirection {
+enum Location {
+	CENTRAL,
+	FOREST,
+	COUNT
+}
+
+enum Direction {
 	UP,
 	DOWN,
 	LEFT,
@@ -14,152 +20,204 @@ enum PathDirection {
 	COUNT
 }
 
-const ROOM_SCENES:Array[PackedScene] = [
-	preload("res://Scenes/Rooms/Prototype1/room_1.tscn"),
-	preload("res://Scenes/Rooms/Prototype1/room_2.tscn"),
-	preload("res://Scenes/Rooms/Prototype1/room_3.tscn"),
-	preload("res://Scenes/Rooms/Prototype1/room_4.tscn")
+const DIRECTION_FROM_CENTRAL:Dictionary = {
+	Location.FOREST: Direction.DOWN
+}
+
+const ROOMS:Dictionary = {
+	Location.CENTRAL: [
+		preload("res://Scenes/Rooms/central_room.tscn")
+	],
+	Location.FOREST: [
+		preload("res://Scenes/Rooms/Forest/room_1.tscn"),
+		preload("res://Scenes/Rooms/Forest/room_2.tscn"),
+		preload("res://Scenes/Rooms/Forest/room_3.tscn"),
+		preload("res://Scenes/Rooms/Forest/room_4.tscn")
+	]
+}
+
+const FOREST_EDGES:Array[PackedScene] = [
+	preload("res://Scenes/Rooms/Forest/up_blocking_edge.tscn"),
+	preload("res://Scenes/Rooms/Forest/down_blocking_edge.tscn"),
+	preload("res://Scenes/Rooms/Forest/left_blocking_edge.tscn"),
+	preload("res://Scenes/Rooms/Forest/right_blocking_edge.tscn")
 ]
 
-const BLOCKING_EDGES:Array[PackedScene] = [
-	preload("res://Scenes/Rooms/Prototype1/up_blocking_edge.tscn"),
-	preload("res://Scenes/Rooms/Prototype1/down_blocking_edge.tscn"),
-	preload("res://Scenes/Rooms/Prototype1/left_blocking_edge.tscn"),
-	preload("res://Scenes/Rooms/Prototype1/right_blocking_edge.tscn")
-]
+var active_location:Location
+var active_room:int
 
-var active_room_index:int = -1
-var instances:Array = []
-var connections:Dictionary = {}
-var blocking_edges:Array = []
+var rooms:Dictionary
+var edges:Dictionary
+
+var connections:Dictionary
 
 func _ready() -> void:
-	for direction in range(PathDirection.COUNT):
-		blocking_edges.append(BLOCKING_EDGES[direction].instantiate())
+	active_location = Location.CENTRAL
+	active_room = 0
 
-func set_active_room(index:int) -> void:
+	rooms[Location.CENTRAL] = [ROOMS[Location.CENTRAL][0].instantiate()]
+	add_connection_entry(Location.CENTRAL, 0)
+
+	rooms[Location.FOREST] = []
+	for room in ROOMS[Location.FOREST]:
+		rooms[Location.FOREST].append(room.instantiate())
+
+	edges[Location.CENTRAL] = []
+	edges[Location.FOREST] = []
+	for edge in FOREST_EDGES:
+		edges[Location.CENTRAL].append(edge.instantiate())
+		edges[Location.FOREST].append(edges[Location.CENTRAL][-1])
+
+func set_active_room(location:Location, room:int) -> void:
 	# Delete the current active room.
-	if active_room_index != -1:
-		remove_child(instances[active_room_index])
-#
+	remove_child(rooms[active_location][active_room])
+
+	# Switch out all of the edges when entering a new location.
+	if location != active_location:
+		for direction in range(Direction.COUNT):
+			if edges[location][direction].is_inside_tree():
+				remove_child(edges[location][direction])
+
 	# Add the new active room.
-	active_room_index = index
-	add_child(instances[active_room_index])
+	active_location = location
+	active_room = room
+	add_child(rooms[active_location][active_room])
 
 	# Update the blocking edges.
-	for direction in range(PathDirection.COUNT):
-		if connections[index][direction] != null and blocking_edges[direction].is_inside_tree():
-			remove_child(blocking_edges[direction])
-		elif connections[index][direction] == null and not blocking_edges[direction].is_inside_tree():
-			add_child(blocking_edges[direction])
+	for direction in range(Direction.COUNT):
+		if connections[location][room][direction]["room"] != null and edges[location][direction].is_inside_tree():
+			remove_child(edges[location][direction])
+		elif connections[location][room][direction]["room"] == null and not edges[location][direction].is_inside_tree():
+			add_child(edges[location][direction])
 
-func add_connection_entry(index:int) -> void:
-	if connections.has(index):
-		print("Attempted to add duplicate room: %" % index)
+func add_connection_entry(location:Location, room:int) -> void:
+	if not connections.has(location):
+		connections[location] = {}
+
+	if connections[location].has(room):
+		print("Attempted to add duplicate room %s in location %s" % [room, location])
 		return
 
 	# Add an empty connection entry for the room.
-	connections[index] = {
-		PathDirection.UP: null,
-		PathDirection.DOWN: null,
-		PathDirection.LEFT: null,
-		PathDirection.RIGHT: null
+	connections[location][room] = {
+		Direction.UP: {
+			"location": Location.COUNT,
+			"room": null
+		},
+		Direction.DOWN: {
+			"location": Location.COUNT,
+			"room": null
+		},
+		Direction.LEFT: {
+			"location": Location.COUNT,
+			"room": null
+		},
+		Direction.RIGHT: {
+			"location": Location.COUNT,
+			"room": null
+		}
 	}
 
+func create_connection(source_location:Location, source_room:int, destination_location:Location, destination_room:int, direction:Direction):
+		connections[source_location][source_room][direction]["location"] = destination_location
+		connections[source_location][source_room][direction]["room"] = destination_room
+
+		# Intentional integer division
+		var opposite_direction:int = ((direction / 2) * 2) + (1 - (direction % 2))
+		connections[destination_location][destination_room][opposite_direction]["location"] = source_location
+		connections[destination_location][destination_room][opposite_direction]["room"] = source_room
+
 func generate_rooms() -> void:
-	# Declare the arrays that we're about to start doing
-	# some VERY questionable things with.
-	var unattached:Array[int]
-	var unexplored:Array[int]
+	for location in range(Location.COUNT):
+		if location == Location.CENTRAL:
+			continue
 
-	# All of the rooms begin unattached.
-	for i in range(ROOM_SCENES.size()):
-		instances.append(ROOM_SCENES[i].instantiate())
-		unattached.append(i)
+		# Declare the arrays that we're about to start doing
+		# some VERY questionable things with.
+		var unattached:Array[int]
+		var unexplored:Array[int]
 
-	# Select a random room to serve as the anchor and move it from
-	# unattached to unexplored.
-	#
-	# Here's a good spot to talk about unattached vs. unexplored:
-	# - Unattached means a room is not linked to any other rooms.
-	# - Unexplored means a room is linked to another room but has
-	#   not made links of its own.
-	var random:RandomNumberGenerator = RandomNumberGenerator.new()
-	var start:int = unattached[random.randi_range(0, unattached.size() - 1)]
-	unattached.erase(start)
-	unexplored.append(start)
-	add_connection_entry(start)
+		# All of the rooms begin unattached.
+		for i in range(ROOMS[location].size()):
+			unattached.append(i)
 
-	while unattached.size() > 0:
-		# Pick a random, unexplored room to... explore.
-		var source:int = unexplored[random.randi_range(0, unexplored.size() - 1)]
+		# Select a random room to serve as the anchor and move it from
+		# unattached to unexplored.
+		#
+		# Here's a good spot to talk about unattached vs. unexplored:
+		# - Unattached means a room is not linked to any other rooms.
+		# - Unexplored means a room is linked to another room but has
+		#   not made links of its own.
+		var random:RandomNumberGenerator = RandomNumberGenerator.new()
+		var start:int = unattached[random.randi_range(0, unattached.size() - 1)]
+		unattached.erase(start)
+		unexplored.append(start)
+		add_connection_entry(location, start)
+		create_connection(Location.CENTRAL, 0, location, start, DIRECTION_FROM_CENTRAL[location])
 
-		# Pick a random number of rooms to link to this one.
-		# There can only be a maximum of three NEW attachments because all
-		# rooms will have a previous link (the anchor is linked to whatever
-		# scene brought us to this area).
-		var attachments:int = random.randi_range(1, PathDirection.COUNT - 2)
-		for i in attachments:
-			# Break if we attempt to make more attachments
-			# than we have unattached rooms.
-			if unattached.size() == 0:
-				break
+		while unattached.size() > 0:
+			# Pick a random, unexplored room to... explore.
+			var source:int = unexplored[random.randi_range(0, unexplored.size() - 1)]
 
-			var direction:PathDirection
-			while true:
-				# Find an random, open direction to link on.
-				# Consider this fully optimized. There is nothing faster than
-				# a while loop that breaks on a random condition.
-				direction = random.randi_range(0, PathDirection.COUNT - 1) as PathDirection
-				if connections[source][direction] == null:
+			# Pick a random number of rooms to link to this one.
+			# There can only be a maximum of three NEW attachments because all
+			# rooms will have a previous link (the anchor is linked to whatever
+			# scene brought us to this area).
+			var attachments:int = random.randi_range(1, Direction.COUNT - 2)
+			for i in attachments:
+				# Break if we attempt to make more attachments
+				# than we have unattached rooms.
+				if unattached.size() == 0:
 					break
 
-			# Pick a random room from the list of unattached rooms to link
-			# to the destination room from the source room.
-			var destination:int = unattached[random.randi_range(0, unattached.size() - 1)]
-			connections[source][direction] = destination
+				var direction:Direction
+				while true:
+					# Find an random, open direction to link on.
+					# Consider this fully optimized. There is nothing faster than
+					# a while loop that breaks on a random condition.
+					direction = random.randi_range(0, Direction.COUNT - 1) as Direction
+					if connections[location][source][direction]["room"] == null:
+						break
 
-			# Link to the source room from the destination room so things don't
-			# get weird when it's this destination's turn to be a source.
-			var opposite_direction:int = ((direction / 2) * 2) + (1 - (direction % 2))
-			add_connection_entry(destination)
-			connections[destination][opposite_direction] = source
+				# Pick a random room from the list of unattached rooms to link
+				# to the destination room from the source room.
+				var destination:int = unattached[random.randi_range(0, unattached.size() - 1)]
+				add_connection_entry(location, destination)
+				create_connection(location, source, location, destination, direction)
 
-			# The destination is now unexplored.
-			unattached.erase(destination)
-			unexplored.append(destination)
+				# The destination is now unexplored.
+				unattached.erase(destination)
+				unexplored.append(destination)
 
-		# The source is now explored.
-		unexplored.erase(source)
+			# The source is now explored.
+			unexplored.erase(source)
 
-	# Activate the starting room now
-	# that we have edge information.
-	set_active_room(start)
+	set_active_room(Location.CENTRAL, 0)
 
 func _on_path_up_body_entered(body: Node2D) -> void:
 	if is_instance_of(body, Player):
-		var destination = connections[active_room_index][PathDirection.UP]
-		if destination != null:
-			set_active_room(destination)
+		var destination = connections[active_location][active_room][Direction.UP]
+		if destination["room"] != null:
+			set_active_room(destination["location"], destination["room"])
 			body.position.y = get_viewport_rect().size.y - body.get_size().y
 
 func _on_path_down_body_entered(body: Node2D) -> void:
 	if is_instance_of(body, Player):
-		var destination = connections[active_room_index][PathDirection.DOWN]
-		if destination != null:
-			set_active_room(destination)
+		var destination = connections[active_location][active_room][Direction.DOWN]
+		if destination["room"] != null:
+			set_active_room(destination["location"], destination["room"])
 			body.position.y = body.get_size().y
 
 func _on_path_left_body_entered(body: Node2D) -> void:
 	if is_instance_of(body, Player):
-		var destination = connections[active_room_index][PathDirection.LEFT]
-		if destination != null:
-			set_active_room(destination)
+		var destination = connections[active_location][active_room][Direction.LEFT]
+		if destination["room"] != null:
+			set_active_room(destination["location"], destination["room"])
 			body.position.x = get_viewport_rect().size.x - body.get_size().x * 2
 
 func _on_path_right_body_entered(body: Node2D) -> void:
 	if is_instance_of(body, Player):
-		var destination = connections[active_room_index][PathDirection.RIGHT]
-		if destination != null:
-			set_active_room(destination)
+		var destination = connections[active_location][active_room][Direction.RIGHT]
+		if destination["room"] != null:
+			set_active_room(destination["location"], destination["room"])
 			body.position.x = body.get_size().x * 2
