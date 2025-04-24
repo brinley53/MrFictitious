74e7bc4a99ce516328da7f49246c398f2d533afe
brinley53 <brinleyhull53@ku.edu>
2025-04-24 10:54:12 -0500

    statue attack animation and death

--- mr-fictitious/Scripts/Enemies/statue.gd
+++ mr-fictitious/Scripts/Enemies/statue.gd
@@ -3,12 +3,17 @@ Script for the statue boss logic
 Authors: Brinley Hull
 Creation Date: 04/15/2025
 Revisions:
+	Brinley Hull - 4/24/2025: 
+		- Animation
+		- Death Logic
+		- Basic Attacks
 """
 extends CharacterBody2D
 #GLOBAL VARIABLES
 # stats attributes
-var left_arm_health : int
-var right_arm_health : int
+var left_wing_health : int
+var right_wing_health : int
+var head_health : int
 var speed : float
 @export var base_speed : float
 @export var damage : float
@@ -16,9 +21,12 @@ var speed : float
 #chase player variables
 var chase_player = false
 var attack_player = false
+var attack_type = "Pound"
+var attack_types = ["Pound", "Charge"]
 
 # On ready attributes
 @onready var timer = $AttackTimer
+@onready var big_attack_timer = $BigAttackTimer
 @onready var sprite = $AnimatedSprite2D
 @onready var detection = $Detection
 @onready var players = get_tree().get_nodes_in_group("Player")
@@ -27,12 +35,14 @@ var attack_player = false
 func _ready():
 	#set initial variables
 	speed = base_speed
-	left_arm_health = 3
-	right_arm_health = 3
+	left_wing_health = 3
+	right_wing_health = 3
+	head_health = 5
+	sprite.play("pound")
 
 func _physics_process(delta: float) -> void:
 	# Die if its arms are off
-	if left_arm_health == 0 and right_arm_health == 0:
+	if left_wing_health <= 0 and right_wing_health <= 0 and head_health <= 0:
 		queue_free()
 
 #When player is inside the Attack Area, Take Damage (Will be change to something more later)
@@ -52,8 +62,6 @@ func _on_attack_area_body_exited(body: Node2D) -> void:
 	# When player escapes, don't reduce health anymore
 	if body.name == "Player":
 		attack_player = false
-		poison_timer.start()
-		current_proc_count = 0
 			
 
 func _on_attack_timer_timeout() -> void:
@@ -62,9 +70,36 @@ func _on_attack_timer_timeout() -> void:
 		player.reduce_player_health(damage)
 		timer.start()
 
-# Functions to decrease health
-func _on_left_arm_body_entered(body: Node2D) -> void:
-	left_arm_health -= 1
 
-func _on_right_arm_body_entered(body: Node2D) -> void:
-	right_arm_health -= 1
+func _on_head_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Weapon"):
+		head_health -= 1
+
+
+func _on_right_wing_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Weapon"):
+		right_wing_health -= 1
+
+
+func _on_left_wing_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Weapon"):
+		left_wing_health -= 1
+
+func ground_pound():
+	sprite.play("pound")
+	
+func charge():
+	pass
+
+func _on_big_attack_timer_timeout() -> void:
+	big_attack_timer.start()
+	if attack_type == "Pound":
+		ground_pound()
+	else:
+		charge()
+		
+	attack_type = attack_types[randi_range(0, 1)];
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	sprite.play("default")
