378ef323aeb7e99b34d025405aa7f0110d0e6737
JoseLeyba <leybajoseluis@ku.edu>
2025-04-28 16:56:35 -0500

    separating title screen and losing screen scripts

--- mr-fictitious/Scripts/losing_screen.gd
+++ mr-fictitious/Scripts/losing_screen.gd
@@ -1,13 +1,4 @@
 extends Control
-
-func _on_reload_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-
-func _hard_reset():
-	get_tree().root.free()
-	var new_scene = load("res://Scenes/main.tscn").instantiate()
-	get_tree().root.add_child(new_scene)
-	get_tree().current_scene = new_scene
 @export var PlayMusic:WwiseEvent
 @export var playTitle:WwiseEvent
 @export var playAgain:WwiseEvent
--- mr-fictitious/Scripts/title_screen.gd
+++ mr-fictitious/Scripts/title_screen.gd
@@ -1,6 +1,7 @@
 extends Control
-
-
+@export var PlayMusic:WwiseEvent
+@export var playTitle:WwiseEvent
+@export var playAgain:WwiseEvent
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/opening_dialogue.tscn")
 
@@ -12,8 +13,26 @@ func _on_button_2_pressed() -> void:
 func _on_glossary_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/enemy_glossary.tscn")
 
-	
-func _on_tree_entered() -> void:
-	print("Put in scene")
 
+func _on_tree_entered() -> void:
+	Wwise.register_game_obj(self,self.name)
+	Wwise.register_listener(self)
+	Wwise.load_bank_id(AK.BANKS.MUSIC)
+	Wwise.load_bank_id(AK.BANKS.SOUND)
+	PlayMusic.post(self)
+	playTitle.post(self)
+	playAgain.post(self)
+	print("Put in scene")
 	
+
+
+func _on_tree_exiting() -> void:
+	
+	PlayMusic.stop(self,200,AkUtils.AK_CURVE_LINEAR)
+	playTitle.stop(self,200,AkUtils.AK_CURVE_LINEAR)
+	playAgain.stop(self,200,AkUtils.AK_CURVE_LINEAR)
+	Wwise.unregister_game_obj(self)
+	Wwise.unload_bank_id(AK.BANKS.SOUND)
+	Wwise.unload_bank_id(AK.BANKS.MUSIC)
+	print("Exiting objects")
+	pass # Replace with function body.
